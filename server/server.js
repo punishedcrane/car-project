917fe1f2cc087aa1f3884c97c864de27eadfcd62
punishedcrane <rockmanx62@gmail.com>
2025-05-03 23:32:12 +0530

    Update backend and frontend

--- server/server.js
+++ server/server.js
@@ -29,7 +29,7 @@ app.use(
 
 // Normal Middlewares
 app.use(cors({
-  origin: process.env.CLIENT_URL || 'http://localhost:5173',
+  origin: process.env.CLIENT_URL || '',
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
 }));
